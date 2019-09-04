1deeba5b068e84991cfd0f4d5ba0410596ac1966
dipadana <putudipadana@gmail.com>
2019-09-04 14:45:54 +0700

    Kumpul createArrayOfObject

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Dipadana Putu
 
 // Release 0
-var arr = [];
+let arr = [];
 
 function createObj(name, phase, gender){
   arr.push({
@@ -20,7 +20,7 @@ console.log(arr)
 
 // Release 1
 function getData(name){
-  for(var i = 0; i < arr.length; i++){
+  for(let i = 0; i < arr.length; i++){
     if(arr[i].name == name){
       return arr[i];
     }
